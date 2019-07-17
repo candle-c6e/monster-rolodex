6fec697188bf61177ea0a08ff33967df64eea7b0
Candles.(POK) <thainvehement@gmail.com>
2019-07-17 23:45:31 +0700

    Updates

---
+++ precache-manifest.54aac6845e02198f6a577fda24d5d7c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563c2962a102c5feec63b11d61679ef0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "01b15a9d740f96e6f1b9",
+    "url": "/monster-rolodex/static/css/main.ecc77ad4.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/monster-rolodex/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "01b15a9d740f96e6f1b9",
+    "url": "/monster-rolodex/static/js/main.cf0659a3.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
